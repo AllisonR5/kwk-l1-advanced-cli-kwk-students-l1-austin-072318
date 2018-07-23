f307b3373f83850a7758b2b4339f58b5eecdcb24
AllisonR5 <allicraft05@gmail.com>
2018-07-23 17:55:40 +0000

    automatically backed up by learn

--- stock_supermarket.sh
+++ stock_supermarket.sh
@@ -2,4 +2,5 @@ ls;
 mv delivery/apples.txt fruit
 mv delivery/bison_burgers.txt meat
 mv delivery/carrot_cake.txt dessert
-mv delvery/carrots.txt veggies
+mv delivery/carrots.txt veggies
+mv delivery/eggo_waffles.txt 
